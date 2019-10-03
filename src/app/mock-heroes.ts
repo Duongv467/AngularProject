b9b06bbcc6932976516775b8be19820a7d555a99
unknown <duongv467@gmail.com>
2019-10-03 11:56:31 -0400

    My first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Hanoi' },
+  { id: 12, name: 'Chantilly' },
+  { id: 13, name: 'Longwood Garden' },
+  { id: 14, name: 'Arbotum Garden' },
+  { id: 15, name: 'Rehoboth Beach' },
+  { id: 16, name: 'Watskin Park' },
+  { id: 17, name: 'Great Fall' },
+  { id: 18, name: 'National Aquarium in MD' },
+  { id: 19, name: 'Cascade Falls' },
+  { id: 20, name: 'Home' }
+];
